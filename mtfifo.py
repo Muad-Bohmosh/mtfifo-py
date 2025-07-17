9edd0d59efd8b6f9a0f395b435bf01191e7f8c7a
Muad <mouad22bh@gmail.com>
2025-07-17 11:29:37 +0300

    Update mtfifo.py

--- mtfifo.py
+++ mtfifo.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2025 Muad Bohmosh
 #
 # This file is part of the MTFIFO project.
-# Released under the MIT License.
-# See LICENSE file in the root of this repository for full license text.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
 
 
 import threading
